2569192204c0b764564d3654aef5ca7822014d43
Matt Oestreich <matthewpoestreich@gmail.com>
2019-06-29 23:35:10 -0500

    Update floyds_triange.py

--- floyds_triange.py
+++ floyds_triange.py
@@ -15,3 +15,42 @@ make_triangle(10)
 #  If you want to allow input.....
 #  print("Enter a number: ", end="")
 #  make_triangle(int(input("")))
+
+
+""" OUTPUT
+1
+
+1 2
+
+1 2 3
+
+1 2 3 4
+
+1 2 3 4 5
+
+1 2 3 4 5 6
+
+1 2 3 4 5 6 7
+
+1 2 3 4 5 6 7 8
+
+1 2 3 4 5 6 7 8 9
+
+1 2 3 4 5 6 7 8 9
+
+1 2 3 4 5 6 7 8
+
+1 2 3 4 5 6 7
+
+1 2 3 4 5 6
+
+1 2 3 4 5
+
+1 2 3 4
+
+1 2 3
+
+1 2
+
+1
+"""
